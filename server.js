ac1b8b59e727a78975d9cd06c45ef7ae27e1f92d
thvvamshi <bodavamshikumar2002@gmail.com>
2026-04-03 23:19:21 +0530

    feat: final update for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
+const PORT = process.env.PORT || 5000;
+
 const startServer = async () => {
   try {
     await connectDB(); // wait for DB
